d934893671c3338e5dc07c84a975e2000e3f221d
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-05-11 21:09:37 -0300

    archivo donde se generaran las personas users

---
+++ src/data_generation/generate_users.py
@@ -0,0 +1,23 @@
+from personas import PERSONAS
+from states import STATES
+from transitions import TRANSITIONS
+
+
+def choose_persona():
+    pass
+
+
+def sample_attributes():
+    pass
+
+
+def generate_users():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
